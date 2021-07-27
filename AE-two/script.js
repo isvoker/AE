fe368da53f0be72f46292a5aba8fed5afa35ddf9
isvoker <84444626+isvoker@users.noreply.github.com>
2021-07-28 06:17:35 +0700

    Add files via upload

---
+++ AE-two/script.js
@@ -0,0 +1,73 @@
+$('.open-popup').click(function(e) {
+    e.preventDefault();
+    $('.popup-bg').fadeIn(800);
+    $('html').addClass('no-scroll');
+});
+
+$('.close-popup').click(function() {
+    $('.popup-bg').fadeOut(800);
+    $('html').removeClass('no-scroll');
+});
+
+var currentTab = 0;
+showTab(currentTab); 
+
+function showTab(n) {
+  var x = document.getElementsByClassName("tab");
+  x[n].style.display = "block";
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline";
+  }
+  if (n == (x.length - 1)) {
+    document.getElementById("nextBtn").innerHTML = "Отправить";
+  } else {
+    document.getElementById("nextBtn").innerHTML = "Вперед";
+  }
+  fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+  var x = document.getElementsByClassName("tab");
+  if (n == 1 && !validateForm()) {
+      return false;
+}
+  x[currentTab].style.display = "none";
+  currentTab = currentTab + n;
+
+  if (currentTab >= x.length) {
+    document.getElementById("regForm").submit();
+    return false;
+  }
+ 
+  showTab(currentTab);
+}
+
+function validateForm() {
+
+  var x, y, i, valid = true;
+  x = document.getElementsByClassName("tab");
+  y = x[currentTab].getElementsByTagName("input");
+
+  for (i = 0; i < y.length; i++) {
+
+    if (y[i].value == "") {
+      y[i].className += " invalid";
+      valid = false;
+    }
+  }
+
+  if (valid) {
+    document.getElementsByClassName("step")[currentTab].className += " finish";
+  }
+  return valid; 
+}
+
+function fixStepIndicator(n) {
+  var i, x = document.getElementsByClassName("step");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  x[n].className += " active";
+}
